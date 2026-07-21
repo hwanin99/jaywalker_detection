55169c1c84bb44d83dc3ec963b4de23f806d2b8a
Kim Sung Hwan <tjdghks0521@naver.com>
2026-07-21 17:45:42 +0900

    Update create_model.py

--- segmentation/utils/create_model.py
+++ segmentation/utils/create_model.py
@@ -13,10 +13,10 @@ def create_model(model_name):
         
     elif model_name == "deeplab_v3_p":
         from models.DeepLabv3_plus import DeepLabv3_plus
-        model = DeepLabv3_plus(n_classes=1,os=16, pretrained=True)
+        model = DeepLabv3_plus(n_classes=1,os=8, pretrained=True)
         return model
 
     elif model_name == "fft_deeplab_v3_p":
         from models.FFT_DeepLabv3_plus import FFT_DeepLabv3_plus
-        model = FFT_DeepLabv3_plus(n_classes=1,os=8, pretrained=True)
+        model = FFT_DeepLabv3_plus(n_classes=1,os=16, pretrained=True)
         return model
